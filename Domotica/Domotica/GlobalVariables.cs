dbac75456d61d8b9c7003a71fb524f4288395329
Pieter <pieter1998.verweij@gmail.com>
2016-01-10 19:25:33 +0100

    Removed: Unused Variables

--- Domotica/Domotica/GlobalVariables.cs
+++ Domotica/Domotica/GlobalVariables.cs
@@ -8,10 +8,6 @@ namespace Domotica
 		static string _IPAddress;
 		static int _PortAddress;
 		static bool _IpAvailable;
-		static int _Sensor1Value;
-		static int _Sensor2Value;
-		static bool _Sensor1Get;
-		static bool _Sensor2Get;
 
 		public static string IPAddress
 		{
@@ -40,46 +36,6 @@ namespace Domotica
 			}
 			set {
 				_IpAvailable = value;
-			}
-		}
-
-		public static int Sensor1Value
-		{
-			get {
-				return _Sensor1Value;
-			}
-			set {
-				_Sensor1Value = value;
-			}
-		}
-
-		public static int Sensor2Value
-		{
-			get {
-				return _Sensor2Value;
-			}
-			set {
-				_Sensor2Value = value;
-			}
-		}
-
-		public static bool Sensor1Get
-		{
-			get {
-				return _Sensor1Get;
-			}
-			set {
-				_Sensor1Get = value;
-			}
-		}
-
-		public static bool Sensor2Get
-		{
-			get {
-				return _Sensor2Get;
-			}
-			set {
-				_Sensor2Get = value;
 			}
 		}
 	}
